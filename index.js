ce6a10e969447b63a4f4967e5b788e05f3575ae9
Seth Barlow <sethdbarlow@gmail.com>
2022-12-09 15:27:07 -0700

    commit tree 30a73f45b50fb72d7e811af6fb82e03d911b19e5

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(arr, eventName) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`);
+    }
+return newArr;
+}
+
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num -= 1;
+    }
+}
+
