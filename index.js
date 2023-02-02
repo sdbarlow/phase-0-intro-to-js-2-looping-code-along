0346bd7bef08d44974d4dee8c3ca3273eafc40c6
Seth Barlow <sethdbarlow@gmail.com>
2023-02-02 09:56:40 -0700

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(arr, verb){
+    const newArr = [];
+    for (let i=0; i<arr.length; i++){
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${verb} gift!`);
+    }
+    return newArr;
+}
+
+console.log(writeCards(names, "surprise"))
+
+
+function countDown(num){
+    for (let i=num; i>=0; i--)
+        console.log(num--)
+}
+
+
